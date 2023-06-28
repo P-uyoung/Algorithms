1d4f8e2daf3eb22ea1f34b3eeec6120d1b1ca180
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-06-28 12:00:06 +0900

    [Silver IV] Title: 듣보잡, Time: 80 ms, Memory: 44060 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,15 +1,7 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-arrN, arrM = set(), set()
-for _ in range(n):
-    arrN.add(input())
-for _ in range(m):
-    arrM.add(input())
-    
-res = list(arrN & arrM)
-res.sort()
-rN = len(res)
-print(rN)
-for i in range(rN):
-    print(res[i])
-               
-    
+x = set(input() for _ in range(n)).intersection(set(input() for _ in range(m)))
+print(len(x))
+print(''.join(sorted(list(x))))
