f258ddfbc6f34ada4924fd8c82c6ce45e074b3cd
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-07-25 20:38:30 +0900

    [Silver III] Title: 주유소, Time: 328 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,14 +1,13 @@
 n = int(input())
-cost = list(map(int, input().split()))
+city = list(map(int, input().split()))
 price = list(map(int, input().split()))[:-1]
 dp = [[0]*n for _ in range(n)]
-for i, cash in enumerate(price):
-    for j, cnt in enumerate(cost):
+for i, cost in enumerate(price):
+    dp[i+1][i] = dp[i][i]
+    for j in range(i,n-1):
         if i == 0:
-            dp[i+1][j+1] = dp[i+1][j] + cash*cnt
-        else:
-            if i > j:
-                dp[i+1][j+1] = dp[i][j+1]
-            else:
-                dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j]+cash*cnt)
+            dp[i+1][j+1] = dp[i+1][j]+cost*city[j]
+
+        else: 
+            dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j]+cost*city[j])
 print(dp[-1][-1])
