d9e587061c0588cff81505ff1c44a8822592aef0
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-08-08 22:50:57 +0900

    [Silver III] Title: 퇴사, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+output = sys.stdout.write
+
+n = int(input())
+schedule = [list(map(int, input().split())) for _ in range(n)]
+dp = [0]*(n+1)
+for i in range(n):
+    for j in range(i+schedule[i][0], n+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+output(str(dp[-1]))
