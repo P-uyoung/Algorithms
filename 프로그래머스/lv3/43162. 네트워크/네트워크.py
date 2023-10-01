033e486aa8957e1c42911392b724e546b5e2eae1
WooyoungPark <48639021+P-uyoung@users.noreply.github.com>
2023-10-01 20:07:36 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,24 @@
+def dfs(k, graph, visited):
+    visited[k] = True
+    [dfs(i, graph, visited) for i in graph[k] if visited[i]==False]
+    return 
+
+        
+def solution(n, computers):
+    answer = 0
+    graph = [[] for i in range(n)]
+
+    for i, arr in enumerate(computers):
+        for j in range(n):
+            if i != j and arr[j]==1:
+                graph[i].append(j)
+    
+    visited = [False for i in range(n)]
+    for i in range(n):
+        if visited[i] == False:
+            answer += 1
+            print(i)
+            dfs(i, graph, visited)
+                
+    print(answer)      
+    return answer
