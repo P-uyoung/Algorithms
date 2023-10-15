7ee892f8d5320b12f49e2b8b8d5f2f6bfe133038
WooyoungPark <48639021+P-uyoung@users.noreply.github.com>
2023-10-16 03:04:32 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -2,33 +2,33 @@ from collections import defaultdict
 
 def solution(tickets):
     path = defaultdict(list)
-    exist = defaultdict(int)   # visited = set() 대신,
+    visited = defaultdict(int)
     for a, b in tickets:
         path[a].append(b)
         # path[b].append(a)
-        exist[(a,b)] += 1
+        visited[(a,b)] += 0
         
     path = {k: sorted(v) for k, v in path.items()}
         
     n = len(tickets)
     # visited = set()  # 동일한 티켓을 고려 못함
     def dfs(start, ans):
-        if sum(exist.values()) == 0:
+        if len(visited) == n:
             return ans
         
         if start not in path:
             return
         
         for end in path[start]:
-            if exist[(start,end)]:
-                exist[(start,end)] -= 1
+            if not visited[(start,end)]:
+                visited[(start,end)] -= 1
                 ans.append(end)
                 
                 result = dfs(end, ans)
                 if result:
                     return result
                 
-                exist[(start,end)] += 1
+                visited[(start,end)] += 1
                 ans.pop()
 
     return dfs('ICN', ['ICN'])
