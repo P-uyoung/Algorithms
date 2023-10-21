d86de9f5cd86a0192d19a7ee9cb3c2ae76bf0486
WooyoungPark <48639021+P-uyoung@users.noreply.github.com>
2023-10-22 00:13:05 +0900

    [level 3] Title: 입국심사, Time: 0.04 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,23 @@
+def solution(n, times):
+    left, right = 1, max(times) * n
+
+    def count(time):
+        nums = 0
+        for a in times:
+            nums += time // a
+        return nums
+
+    answer = 0
+    while left <= right:
+        mid = (left + right) // 2
+        counted = count(mid)
+        if n == counted:
+            answer = mid  # Save the result if the count matches
+            right = mid - 1  # Still try to find a smaller value
+        elif n < counted:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    # If no exact match is found, return the saved result or the left value
+    return answer if answer else left
